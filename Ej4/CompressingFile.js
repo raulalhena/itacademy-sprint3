363a77f3a223eade3913e6356169b22232945b2d
Raul <raul.lopezm@gmail.com>
2021-04-26 16:15:07 +0200

    Ej4 Comprimiendo archivos

---
+++ Ej4/CompressingFile.js
@@ -0,0 +1,42 @@
+/*
+
+Nivel 2 Ej1: Comprimiendo archivo
+
+*/
+
+import fs from "fs";
+import zlib from "zlib";
+import readline from "readline";
+import { pipeline } from "stream";
+
+const zip = zlib.createGzip();
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("¿Qué archivo quieres comprimir? \n", (answer) => {
+    if(fs.existsSync(answer)){
+        compressFile(answer);
+        rl.close();
+    }else{
+        console.log("El archivo no existe o no se puede leer.");
+        rl.close();
+    }
+})
+
+
+function compressFile(file){
+    let source = fs.createReadStream(file,"utf-8");
+    let target = fs.createWriteStream(`${file}_output.zip`, "utf-8");
+
+    pipeline(source,zip, target, (err)=>{
+        if(err){
+            console.log(`Ha ocurrido un error: ${err}`);
+        }else{
+            console.log(`Se ha comprimido el archivo correctamente, lo encontrarás en: ./${file}_output.zip`, "\n");
+        }
+    });
+    return;
+}
