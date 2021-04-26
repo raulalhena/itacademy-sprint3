3bfba1595e4c94a75c1a28bb2a1f75ec502c40e9
Raul <raul.lopezm@gmail.com>
2021-04-26 12:41:41 +0200

    Ej2 Escritura en archivo

---
+++ Ej2/FileWritting.js
@@ -0,0 +1,41 @@
+/*
+
+Nivel 1 Ej2: Escribir en archivo
+
+*/
+
+import fs from "fs";
+import readline from "readline";
+
+const file = "FunctionName.txt";
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+  if(fs.existsSync(file)){
+    rl.question(`El archivo ${file}, ya existe. ¿Quieres sobreescribirlo? (y/n)`, (answer) => {
+        if(answer === 'y'){
+            writeFile();
+            rl.close();
+            return;
+        }
+        console.log("Perfecto! No se ha realizado ninguna acción.");
+        
+    });
+  }else{
+        console.log("El archivo no existía, se va a crear.");
+        writeFile();
+        rl.close();
+  }
+
+function writeFile(){
+    fs.writeFileSync(file, writeFile.name);
+    console.log(`Se ha escrito correctamente en el archivo: ${file}`);
+    console.log(`El contenido del archivo ${file} es: ` + fs.readFileSync(file));
+    return;
+}
+
+
+
