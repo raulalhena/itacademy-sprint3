07f174b6da77dbbb22390908c8f9ddd440920e4e
Raul <raul.lopezm@gmail.com>
2021-04-27 10:41:22 +0200

    Ej3. Refactoring mejora del código. Lectura de archivo

--- Ej3/FileReading.js
+++ Ej3/FileReading.js
@@ -7,21 +7,27 @@ Nivel 1 Ej3: Lectura de fichero
 import fs from "fs";
 import readline from "readline";
 
-const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
+
+const fileMng = function(readFile){
+    const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+
+    rl.question("¿Qué archivo quieres leer? \n", (answer) =>{
+        if(fs.existsSync(answer) && !fs.statSync(answer).isDirectory() && !fs.statSync(answer).isSymbolicLink()){
+            readFile(answer);
+        }else{
+            console.log(`El archivo ${answer} no existe o es incorrecto, crealo e introduce contenido para poder leerlo.`);
+        }
+        rl.close();
     });
-
-rl.question("¿Qué archivo quieres leer? \n", (answer) =>{
-    if(fs.existsSync(answer)){
-        readFile(answer);
-    }else{
-        console.log(`El archivo ${answer} no existe, crealo e introduce contenido para poder leerlo.`);
-    }
-    rl.close();
-});
-
+}
 
 function readFile(file){
-    console.log(`El contenido de ${file}: \n` + fs.readFileSync(file, "utf-8"));
+    console.log(`El contenido de -> ${file}: \n` + fs.readFileSync(file, "utf-8"));
 }
+
+fileMng(readFile);
+
+
