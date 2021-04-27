19aab9ff790f6917489d2e833a7421c0ca951bc8
Raul <raul.lopezm@gmail.com>
2021-04-27 10:02:47 +0200

    Ej1. Refactoring, mejora del código. Mensaje recursivo

--- Ej1/MensajeDelay.js
+++ Ej1/MensajeDelay.js
@@ -6,8 +6,15 @@ Nivel 1 Ej1: Mensaje recursivo con delay
 
 let count = 0;
 
-const intervalo = setInterval(function() {
-    count++;
-    console.log(`Mensaje recursivo: ${count}`);
-}, 1000);
+const interval = function(writeMsg){ 
+    setInterval(function() {
+        count++;
+        writeMsg(count);
+    }, 1000);
+}
 
+const writeMsg = function(){
+    console.log(`Mensaje recursivo: ${count}`);
+}
+
+interval(writeMsg);
